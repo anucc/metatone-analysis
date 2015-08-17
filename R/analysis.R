902c0d3c7f9cecd8a36aaf824841ca16b5a2aa5d
Charles Martin <cpm@charlesmartin.com.au>
2015-08-17 21:49:33 +1000

    fixed up the data file and tidied up the R file for meeting.

--- R/analysis.R
+++ R/analysis.R
@@ -4,18 +4,19 @@ library(ggplot2)
 ## time,filename,number_performers,performance_context,performance_type,instruments,notes,video_location,flux,number_performers,length_seconds,entropy,filename
 df <- read.csv("../metatone-performance-data.csv")
 df$time <- strptime(as.character(df$time), "%Y-%m-%d %H:%M:%S.%OS")
+summary(df)
 
 ## rehearsals/performances/studies only
 reh.perf.studies.df <- subset(df, performance_context %in% c("rehearsal", "performance", "study"))
+reh.perf.studies.df <- subset(reh.perf.studies.df, performance_type %in% c("composition","improvisation"))
 summary(reh.perf.studies.df$performance_type)
-
+summary(reh.perf.studies.df)
 ## plots
 
 ggplot(reh.perf.studies.df, aes(performance_type)) + geom_bar(aes(fill = performance_context))
 ggplot(reh.perf.studies.df, aes(performance_type, flux)) + geom_boxplot(aes(fill = performance_context))
 ggplot(reh.perf.studies.df, aes(performance_type, entropy)) + geom_boxplot(aes(fill = performance_context))
-ggplot(df, aes(performance_context, flux)) + geom_boxplot()
-ggplot(df, aes(performance_context, entropy)) + geom_boxplot()
+
 
 ggplot(df, aes(flux, entropy)) + geom_point(aes(colour = instruments)) + facet_wrap(~instruments)
 ggplot(df, aes(flux, entropy)) + geom_point(aes(size = number_performers, colour = performance_type))
@@ -31,3 +32,6 @@ summary(aov(entropy~performance_context*performance_type, reh.perf.studies.df))
 
 ggplot(reh.perf.studies.df,aes(performance_type,entropy)) + geom_boxplot()
 ggplot(reh.perf.studies.df,aes(performance_type,flux)) + geom_boxplot()
+
+ggplot(reh.perf.studies.df, aes(performance_context, flux)) + geom_boxplot()
+ggplot(reh.perf.studies.df, aes(performance_context, entropy)) + geom_boxplot()
