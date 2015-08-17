4e0a2ed52321b5a8247e6c4232e20cea0b1a1be0
Charles Martin <cpm@charlesmartin.com.au>
2015-08-17 17:22:43 +1000

    adding a few more plots.
    the subset thing doesn’t seem to work :-(

--- R/analysis.R
+++ R/analysis.R
@@ -4,30 +4,30 @@ library(ggplot2)
 ## time,filename,number_performers,performance_context,performance_type,instruments,notes,video_location,flux,number_performers,length_seconds,entropy,filename
 df <- read.csv("../metatone-performance-data.csv")
 df$time <- strptime(as.character(df$time), "%Y-%m-%d %H:%M:%S.%OS")
-## rehearsals/performances only
-reh.perf.df <- subset(df, performance_context %in% c("rehearsal", "performance", "study"))
 
-summary(reh.perf.df$performance_type)
+## rehearsals/performances/studies only
+reh.perf.studies.df <- subset(df, performance_context %in% c("rehearsal", "performance", "study"))
+summary(reh.perf.studies.df$performance_type)
 
 ## plots
 
-ggplot(reh.perf.df, aes(performance_type)) + geom_bar(aes(fill = performance_context))
-
-ggplot(reh.perf.df, aes(performance_type, flux)) + geom_boxplot(aes(fill = performance_context))
-
-ggplot(reh.perf.df, aes(performance_type, entropy)) + geom_boxplot(aes(fill = performance_context))
-
+ggplot(reh.perf.studies.df, aes(performance_type)) + geom_bar(aes(fill = performance_context))
+ggplot(reh.perf.studies.df, aes(performance_type, flux)) + geom_boxplot(aes(fill = performance_context))
+ggplot(reh.perf.studies.df, aes(performance_type, entropy)) + geom_boxplot(aes(fill = performance_context))
 ggplot(df, aes(performance_context, flux)) + geom_boxplot()
+ggplot(df, aes(performance_context, entropy)) + geom_boxplot()
 
+ggplot(df, aes(flux, entropy)) + geom_point(aes(colour = instruments)) + facet_wrap(~instruments)
 ggplot(df, aes(flux, entropy)) + geom_point(aes(size = number_performers, colour = performance_type))
-
 ggplot(df, aes(flux, number_performers)) + geom_point(aes(colour = performance_type))
-
 ggplot(df, aes(flux, entropy)) + geom_point(aes(size = number_performers, colour = performance_type)) + facet_wrap(~performance_context)
-
 ggplot(df, aes(time, flux)) + geom_point(aes(size = number_performers, colour = performance_type))
 
 ## stats
 
-aov(flux~performance_context*performance_type, reh.perf.df)
-aov(entropy~performance_context*performance_type, reh.perf.df)
+## Question: Do Performance Type and Context have a significant effect on Flux and Entropy?
+summary(aov(flux~performance_context*performance_type, reh.perf.studies.df))
+summary(aov(entropy~performance_context*performance_type, reh.perf.studies.df))
+
+ggplot(reh.perf.studies.df,aes(performance_type,entropy)) + geom_boxplot()
+ggplot(reh.perf.studies.df,aes(performance_type,flux)) + geom_boxplot()
