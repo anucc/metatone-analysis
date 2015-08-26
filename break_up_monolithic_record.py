d585bde871337c1b80bcab1f0869cfbc87a4133e
Charles Martin <cpm@charlesmartin.com.au>
2015-08-26 17:02:10 +1000

    more data splits

--- break_up_monolithic_record.py
+++ break_up_monolithic_record.py
@@ -1,10 +1,16 @@
 """
 Split up a touch frame into chunks separated by 30s of inactivity.
 """
-touches = pd.read_csv("data/2015-03-20T13-39-35-MetatoneOSCLog-6PlayerRehearsal-touches.csv", index_col='time', parse_dates=True)
+from __future__ import print_function
+import os
+import pandas as pd
+import numpy as np
+import time
+import datetime
+touches = pd.read_csv("data/2013-04-20T14-55-00-MetatoneOSCLog-touches-posthoc-gestures.csv", index_col='time', parse_dates=True)
 touches["timediff"] = touches.index
 touches["timediff"] = touches["timediff"] - touches["timediff"].shift()
-long_break = touches.ix[touches["timediff"] > pd.to_timedelta("00:00:30")]
+long_break = touches.ix[touches["timediff"] > pd.to_timedelta("00:02:00")]
 #touches.ix[touches.index.indexer_between_time(long_break.index[1],long_break.index[2])]
 first_touch = touches[:1].index[0]
 frame_list = []
