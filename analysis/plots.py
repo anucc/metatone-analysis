d2e6aef0f7580aa6842327a3a8360da5bf3d80d3
Ben Swift <ben@benswift.me>
2016-07-06 17:32:31 +1000

    lots of messing around, no real progress

--- analysis/plots.py
+++ analysis/plots.py
@@ -1,21 +1,33 @@
 import pandas as pd
 # from bokeh.models import HoverTool
 from bokeh.plotting import figure, show, output_file
-from bokeh.charts import Scatter
+from bokeh.charts import Scatter, HeatMap
 
-# filenames = ["../data/" + f
-#              for f in os.listdir("../data")
-#              if f.endswith("-touches-posthoc-gestures.csv")]
-
+# read metadata about the performaces
 metadata = pd.read_csv("../metadata/metatone-performance-information.csv",
                        index_col='time', parse_dates=True)
 
-# we'll build a big fat DataFrame with all the trimmings
-def metatone_df_from_filename(filename):
+def read_gesture_csv(filename):
   """
   read in the data for a specific performance
   """
   csv_path = "../data/" + filename + "-touches-posthoc-gestures.csv"
-  return pd.read_csv(csv_path, index_col='time', parse_dates=True)
+  df = pd.read_csv(csv_path, index_col='time', parse_dates=True)
+  df.insert(0, 'session', filename)
+  # use "melted" format - useful for Bokeh plotting
+  # df = pd.melt(df, id_vars='time', var_name='musician', value_name='gesture')
+  return df
+
+# futzing about
+
+x = read_gesture_csv("2013-04-20T14-55-00-MetatoneOSCLog")
+
+y = [read_gesture_csv(f) for f in metadata['filename']]
+
+x.merge(metadata, on='filename') # it's aliiiiive!
+
+grouped = metadata.groupby('filename')
+
+grouped.agg(read_gesture_csv)
 
 # TODO use multiindexes - http://pandas.pydata.org/pandas-docs/stable/advanced.html
